41f8501efaeb39dcad7a88911077db456a46ec66
Jasbeer Singh <38327762+jass1313@users.noreply.github.com>
2018-04-19 14:24:55 +0530

    update

---
+++ Playgrounds/Functions.playground/Contents.swift
@@ -0,0 +1,18 @@
+//Create Func
+func nameOfFunctions() {
+}
+//Creating the Func For Give Order to robote Buy Milk
+func getMilk() {
+    print("go to shops")
+    print("buy 2 cartons of Milk of Amul Brand")
+    print("pay 80 Ruppes")
+    print("come home")
+    
+}
+
+//Calling the getMilk() function (No need to give instructions everytime for buy milk just give Command getMilk())
+getMilk()
+
+
+
+
